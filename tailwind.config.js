7f9b61b6b03de38a79786ff2e863350a523af64b
AtanasovCode <a_ljupce@yahoo.com>
2024-09-13 13:53:02 +0200

    completed setup of theme colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "custom-purple-100": "hsl(254, 88%, 90%)",
+        "custom-purple-500": "hsl(256, 67%, 59%)",
+
+        "custom-yellow-100": "hsl(31, 66%, 93%)",
+        "custom-yellow-500": "hsl(39, 100%, 71%)",
+
+        "custom-black": "hsl(0, 0%, 7%)",
+        "custom-white": "hsl(0, 0%, 100%)"
+      },
+      fontFamily: {
+        "sans": "DM Sans"
+      }
+    },
   },
   plugins: [],
 }
